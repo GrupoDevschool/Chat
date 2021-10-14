8947cb9c2e9f1f6f7701439ea691a242a887b77f
Lucas Gonçalves <front.end.lb@gmail.com>
2021-10-14 13:30:43 -0300

    atualização no model de room

--- src/models/Room.ts
+++ src/models/Room.ts
@@ -1,11 +1,22 @@
-import mongoose, { Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 
 const roomSchema = new Schema({
-  nome: String,
-  connections: [{ socketId: String, userId: String }],
-  mensagens: [{ texto: String, autorId: String, dataCriacao: Date }],
+  nome: { type: String, required: true },
+  connections: [
+    {
+      socketId: { type: String, required: true },
+      userId: { type: String, required: true },
+    },
+  ],
+  mensagens: [
+    {
+      texto: { type: String, required: true },
+      autorId: { type: String, required: true },
+      dataCriacao: { type: Date, default: new Date() },
+    },
+  ],
 });
 
-const Room = mongoose.model("Room", roomSchema);
+const Room = model("Room", roomSchema);
 
 export default Room;

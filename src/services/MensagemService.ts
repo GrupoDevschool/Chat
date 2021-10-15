47fdf6960e7693f6641de7f0d3d55f245d987f7c
Brunno98 <brunnosantana98@gmail.com>
2021-10-14 23:07:23 -0300

    salvar ultima mensagem na sala

--- src/services/MensagemService.ts
+++ src/services/MensagemService.ts
@@ -1,14 +1,21 @@
 import { Mensagem as MensagemModel } from '../models/Mensagem';
 import { Mensagem } from '../interfaces/Mensagem';
-import { ObjectId, Schema } from 'mongoose';
+import { Room } from '../models/Room';
 
 async function salvarMensagem(msg: Mensagem) {
     const mensagem = new MensagemModel(msg);
-    return await mensagem.save();
+    const mensagemCriada = await mensagem.save();
+    await Room.findByIdAndUpdate(
+        mensagem.room,
+        {$set: {
+            ultimaMensagem: mensagem._id
+        }
+    });
+    return mensagemCriada;
 };
 
 async function buscarMensagemPorGrupo(roomId: string) {
-    const mensagens = await MensagemModel.find(/* { roomId } */)
+    const mensagens = await MensagemModel.find({ roomId })
         .populate('room')
         .populate({path: 'autor', select: 'nome _id'})
         .exec();
